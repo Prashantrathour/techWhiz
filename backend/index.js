654ec244af67e3bf8f5f1a55cd3062ee31d6e5c7
prajit96 <santraprajit@gmail.com>
2023-06-24 12:01:29 +0530

    home page update

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,12 @@ const { config } = require("./config/db")
 const { userRouter } = require("./Routes/user.routes")
 
 const { auth } = require("./middleware/auth")
-
+const cors =require("cors")
 const cookieParser = require('cookie-parser');
 require("dotenv").config()
 
 const app=express()
+app.use(cors())
 app.use(cookieParser());
 app.use(express.json())
 
